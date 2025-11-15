6489a86ad0e5be57c6c343fabf35ff6b56eaf985
MaryamArif7 <www.maryamarif28242@gmail.com>
2025-11-15 20:57:01 +0500

    not getting date not the exact one from spotify

--- server/dist/functions/topSongs.js
+++ server/dist/functions/topSongs.js
@@ -2,16 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.topSongs = void 0;
 const getAccessToken_1 = require("./getAccessToken");
-const topSongs = async () => {
-    const token = await (0, getAccessToken_1.getAccessToken)();
-    console.log(token);
-    const res = await fetch('https://api.spotify.com/v1/playlists/37i9dQZEVXbMDoHDwVN2tF/tracks?limit=10', {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
-    const tracks = await res.json();
-    console.log(tracks);
-    return tracks;
+const topSongs = async (req, res) => {
+    try {
+        const token = await (0, getAccessToken_1.getAccessToken)();
+        console.log('Token:', token.substring(0, 20) + '...');
+        const response = await fetch('https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n', {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        const tracks = await response.json();
+        console.log('Tracks fetched:', tracks.tracks?.items?.length);
+        return res.json(tracks);
+    }
+    catch (error) {
+        console.error('Error:', error.message);
+        return res.status(500).json({ error: error.message });
+    }
 };
 exports.topSongs = topSongs;
--- server/src/functions/topSongs.ts
+++ server/src/functions/topSongs.ts
@@ -1,16 +1,25 @@
 import { getAccessToken } from "./getAccessToken";
-export const topSongs=async ()=>{
-    const token=await getAccessToken();
-    console.log(token);
-  
-   const res=await fetch('https://api.spotify.com/v1/playlists/37i9dQZEVXbMDoHDwVN2tF/tracks?limit=10',
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }
-   )
-   const tracks=await res.json();
-   console.log(tracks);
-   return tracks;
-}
+import { Request, Response } from "express";
+
+export const topSongs = async (req: Request, res: Response) => {
+  try {
+    const token = await getAccessToken();
+    const response = await fetch(
+      'https://api.spotify.com/v1/playlists/3cEYpjA9oz9GiPac4AsH4n',
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    
+    const tracks = await response.json(); 
+    console.log('Tracks fetched:', tracks.tracks?.items?.length);
+    
+    return res.json(tracks);
+    
+  } catch (error: any) {
+    console.error('Error:', error.message);
+    return res.status(500).json({ error: error.message });
+  }
+};
